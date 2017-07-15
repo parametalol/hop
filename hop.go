ec7bc15f5df8ef4c3bc6d963839bb3c64a678c12
mikhail <mikhail.petrov@amadeus.com>
2017-07-15 12:37:27 +0200

    the code

---
+++ hop.go
@@ -0,0 +1,284 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "io"
+    "io/ioutil"
+    "net"
+    "net/http"
+    "net/url"
+    "time"
+    "log"
+    "strings"
+    "strconv"
+    "math/rand"
+)
+
+type hopHandler struct {}
+
+var help = "-wait:[ms], -headers, -size:[bytes]"
+var quit = make(chan int)
+
+func callURL(url string, headers map[string]string, r []string) (*http.Response, error) {
+    client := &http.Client{ }
+    res, err := client.Get(url)
+    if err != nil {
+        return nil, err
+    }
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        return nil, err
+    }
+    for h, v := range headers {
+        req.Header.Set(h, v)
+    }
+    res, err = client.Do(req)
+    if err != nil {
+        return nil, err
+    }
+    return res, nil
+}
+
+func (handler hopHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+
+    var result [1]string
+
+    hn, _ := os.Hostname()
+    path := req.URL.Path
+    if len(req.URL.RawPath) > 0 {
+        path = req.URL.RawPath
+    }
+    result[0] = fmt.Sprintf("I am %s, will do %s", hn, path)
+    r := result[:]
+
+    split := strings.SplitN(path, "/", 3)
+    next_cmd := path
+    if len(split) > 1 {
+        next_cmd = split[1]
+    }
+    path = ""
+    if len(split) > 2 {
+        path = split[2]
+    }
+
+    var headers = map[string]string {
+        "Content-type": "text/plain",
+    }
+    var rheaders = map[string]string {
+        "Content-type": "text/plain",
+    }
+    var fheaders []string
+
+    showHeaders := false
+    skip := false
+    q := false
+    code := 200
+
+    for strings.HasPrefix(next_cmd, "-") {
+        cmd := strings.SplitN(next_cmd, ":", 2)
+
+        split := strings.SplitN(path, "/", 2)
+        if len(split) > 0 {
+            next_cmd = split[0]
+        } else {
+            next_cmd = ""
+        }
+        if len(split) > 1 {
+            path = split[1]
+        } else {
+            path = ""
+        }
+
+        if skip {
+            r = append(r, fmt.Sprintf("Skipping %s", cmd[0]))
+            skip = false
+            continue
+        }
+
+        switch cmd[0] {
+        case "-wait":
+            if len(cmd) != 2 {
+                r = append(r, fmt.Sprintf("Missing parameter for %s", cmd[0]))
+                continue
+            }
+            d, err := strconv.Atoi(cmd[1])
+            if err != nil {
+                r = append(r, fmt.Sprintf("Cannot wait for %s ms", cmd[1]))
+                continue
+            }
+            time.Sleep(time.Duration(d) * time.Millisecond)
+            r = append(r, fmt.Sprintf("Waited for %d ms", d))
+        case "-headers":
+            showHeaders = true
+            r = append(r, "Got headers:")
+            for h, v := range req.Header {
+                r = append(r, fmt.Sprintf(".\t%s: %s", h, v))
+            }
+        case "-header":
+            if len(cmd) != 2 {
+                r = append(r, fmt.Sprintf("Missing parameter for %s", cmd[0]))
+                continue
+            }
+            hv := strings.SplitN(cmd[1], "=", 2)
+            if len(hv) != 2 {
+                r = append(r, fmt.Sprintf("Missing header value for %s", cmd[0]))
+                continue
+            }
+            value, err := url.PathUnescape(hv[1])
+            if err != nil {
+                r = append(r, fmt.Sprintf("Bad value for header %s: %s", hv[0], hv[1]))
+            } else {
+                r = append(r, fmt.Sprintf("Will add header %s: %s", hv[0], value))
+                headers[hv[0]] = value
+            }
+        case "-rheader":
+            if len(cmd) != 2 {
+                r = append(r, fmt.Sprintf("Missing parameter for %s", cmd[0]))
+                continue
+            }
+            hv := strings.SplitN(cmd[1], "=", 2)
+            if len(hv) != 2 {
+                r = append(r, fmt.Sprintf("Missing header value for %s", cmd[0]))
+                continue
+            }
+            value, err := url.PathUnescape(hv[1])
+            if err != nil {
+                r = append(r, fmt.Sprintf("Bad value for header %s: %s", hv[0], hv[1]))
+            } else {
+                r = append(r, fmt.Sprintf("Will return header %s: %s", hv[0], value))
+                rheaders[hv[0]] = value
+            }
+        case "-fheader":
+            if len(cmd) != 2 {
+                r = append(r, fmt.Sprintf("Missing parameter for %s", cmd[0]))
+                continue
+            }
+            r = append(r, fmt.Sprintf("Will forward header %s: %s", cmd[1], req.Header.Get(cmd[1])))
+            headers[cmd[1]] = req.Header.Get(cmd[1])
+            fheaders = append(fheaders, cmd[1])
+        case "-code":
+            if len(cmd) != 2 {
+                r = append(r, fmt.Sprintf("Missing parameter for %s", cmd[0]))
+                continue
+            }
+            c, err := strconv.Atoi(cmd[1])
+            if err != nil {
+                r = append(r, fmt.Sprintf("Cannot return code %s", cmd[1]))
+                continue
+            }
+            code = c
+            r = append(r, fmt.Sprintf("Returning code %d", code))
+        case "-size":
+            if len(cmd) != 2 {
+                r = append(r, fmt.Sprintf("Missing parameter for %s", cmd[0]))
+                continue
+            }
+            b, err := strconv.Atoi(cmd[1])
+            if err != nil {
+                r = append(r, fmt.Sprintf("Cannot create a byte array of %s bytes", cmd[1]))
+                continue
+            }
+            r = append(r, fmt.Sprintf("Appending %d bytes", b))
+            r = append(r, strings.Repeat("X", b))
+            r = append(r, "\n")
+        case "-rnd":
+            if len(cmd) != 2 {
+                r = append(r, fmt.Sprintf("Missing parameter for %s", cmd[0]))
+                continue
+            }
+            p, err := strconv.Atoi(cmd[1])
+            if err != nil {
+                r = append(r, fmt.Sprintf("Cannot make a random of %s", cmd[1]))
+                continue
+            }
+            if rand.Intn(100) < p {
+                skip = true
+                continue
+            }
+        case "-quit":
+            r = append(r, "Quitting")
+            q = true
+        case "-crash":
+            return
+        }
+    }
+
+    if skip {
+        r = append(r, fmt.Sprintf("Skipping call to %s", next_cmd))
+    } else if len(next_cmd) > 0 {
+        url := fmt.Sprintf("http://%s/%s", next_cmd, path)
+        res, err := callURL(url, headers, r)
+        if err != nil {
+            r = append(r, fmt.Sprintf("Couldn't call %s. Got error: %s\n", url, err.Error()))
+        } else {
+            r = append(r, fmt.Sprintf("Called %s with status %s", url, res.Status))
+            if res.Body != nil {
+                data, err := ioutil.ReadAll(res.Body)
+                res.Body.Close()
+                if err != nil {
+                    r = append(r, err.Error())
+                    r = append(r, "\n")
+                } else {
+                    r = append(r, "With data:")
+                    for _, line := range strings.Split(string(data), "\n") {
+                        r = append(r, fmt.Sprintf(".\t%s", line))
+                    }
+                }
+            }
+            for _, h := range fheaders {
+                v := res.Header.Get(h)
+                r = append(r, fmt.Sprintf("Back forwarding header %s: %s", h, v))
+                if len(v) > 0 {
+                    rheaders[h] = v
+                }
+            }
+            if showHeaders {
+                r = append(r, "With headers:")
+                for h, v := range res.Header {
+                    r = append(r, fmt.Sprintf(".\t%s: %s", h, v))
+                }
+            }
+        }
+    }
+
+    for h, v := range rheaders {
+        w.Header().Set(h, v)
+    }
+    w.WriteHeader(code)
+    for _, line := range r {
+        io.WriteString(w, "| ")
+        io.WriteString(w, line)
+        io.WriteString(w, "\n")
+    }
+    if q {
+        quit<-0
+    }
+}
+
+
+func main() {
+    rand.Seed(13)
+    port := os.Getenv("PORT");
+    if len(port) == 0 {
+        port = "8000"
+    }
+    fmt.Printf("Serving on %s\n", port)
+
+    s := &http.Server {
+        Addr: net.JoinHostPort("", port),
+        Handler: hopHandler {},
+        ReadTimeout: 10 * time.Second,
+        WriteTimeout: 10 * time.Second,
+        MaxHeaderBytes: 1 << 20,
+    }
+    go func() {
+        log.Fatal(s.ListenAndServe())
+    }()
+
+    <-quit
+    if err := s.Shutdown(nil); err != nil {
+        panic(err)
+    }
+}
+
