6fdbdf841a1ff4b422bb1bb06d359636da34bc32
mikhail <mikhail.petrov@amadeus.com>
2017-07-21 15:39:27 +0200

    https, command line arguments

--- hop.go
+++ hop.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+    "bytes"
     "context"
+    "crypto/tls"
+    "crypto/x509"
+    "errors"
+    "flag"
     "fmt"
     "io"
     "io/ioutil"
+    "log"
     "math/rand"
     "net"
     "net/http"
@@ -30,25 +36,72 @@ var help = map[string]string {
     "-quit": "stops the server with a nice response",
     "-rheader:H=V": "add header H: V to the reponse",
     "-rnd:P": "execute next command with P% probability",
-    "-size:B": "add B bytes of payload to the response",
+    "-rsize:B": "add B bytes of payload to the response",
+    "-size:B": "add B bytes of payload to the following query",
     "-wait:T": "wait for T ms before response",
 }
 
 var quit = make(chan int)
 
-func callURL(url string, headers map[string]string, r []string) (*http.Response, error) {
-    req, err := http.NewRequest("GET", url, nil)
+var transport *http.Transport
+var tlsClientConfig *tls.Config
+var tlsServerConfig *tls.Config
+
+func initTLS(cacert, cert, key string) {
+    log.Println("Initializing TLS")
+    roots := x509.NewCertPool()
+    data, err := ioutil.ReadFile(cacert)
+    if err != nil {
+        log.Fatalf("failed to load root certificate: %s", err)
+    }
+    ok := roots.AppendCertsFromPEM(data)
+    if !ok {
+        log.Panicln("failed to parse root certificate")
+    }
+    tlsCert, err := tls.LoadX509KeyPair(cert, key)
+    if err != nil {
+        panic(fmt.Sprintf("failed to load client certificate or key: %s", err))
+    }
+    tlsClientConfig = &tls.Config {
+        RootCAs: roots,
+        Certificates: []tls.Certificate { tlsCert },
+        InsecureSkipVerify: true,
+    }
+    tlsServerConfig = &tls.Config {
+        ClientCAs: roots,
+        //RootCAs: roots,
+    }
+    transport = &http.Transport{
+        MaxIdleConns:       10,
+        IdleConnTimeout:    30 * time.Second,
+        DisableCompression: true,
+        TLSClientConfig:    tlsClientConfig,
+    }
+}
+
+func callURL(url *url.URL, headers *map[string]string, size int) (*http.Response, error) {
+    log.Printf("Call %s, sending %d bytes and %s", url, size, headers)
+    payload := bytes.Repeat([]byte{'X'}, size)
+    req, err := http.NewRequest("GET", url.String(), bytes.NewReader(payload))
     if err != nil || req == nil {
         return nil, err
     }
-    for h, v := range headers {
+    for h, v := range *headers {
         req.Header.Set(h, v)
     }
-    client := &http.Client{ }
+    var client *http.Client
+    if url.Scheme == "http" {
+        client = &http.Client{}
+    } else if url.Scheme == "https" {
+        client = &http.Client{ Transport: transport }
+    } else {
+        return nil, errors.New(fmt.Sprintf("Unknown schema %s", url.Scheme))
+    }
     return client.Do(req)
 }
 
 func (handler hopHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+    log.Printf("Received %s", req.URL)
 
     var result [1]string
 
@@ -82,6 +135,7 @@ func (handler hopHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
     skip := false
     not := false
     code := 0
+    size := 0
 
     q := func (c int) {
         quit <- c
@@ -119,7 +173,7 @@ func (handler hopHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
             r = append(r, "curl -H \"a: b\" hop1/-fheader:a/hop2")
             r = append(r, "\tthis will call hop1 which will call hop2 with forwarded header A")
             r = append(r, "curl hop1/-rnd:50/hop2/hop3/-on:hop2/-code:500")
-            r = append(r, "\tthis will call hop1 which will call hop2 or hop3 (50%). hop2 would call hop3 and return error code 500.")
+            r = append(r, "\tthis will call hop1 which will call hop2 or hop3 (50%). hop2 would call hop3 and return error code 500")
         case "-wait":
             if len(cmd) != 2 {
                 r = append(r, fmt.Sprintf("Missing parameter for %s", cmd[0]))
@@ -134,7 +188,7 @@ func (handler hopHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
             r = append(r, fmt.Sprintf("Waited for %d ms", d))
         case "-info":
             showHeaders = true
-            r = append(r, fmt.Sprintf("%d bytes From %s", req.ContentLength, req.RemoteAddr))
+            r = append(r, fmt.Sprintf("Got %d bytes from %s", req.ContentLength, req.RemoteAddr))
             r = append(r, fmt.Sprintf("%s %s %s", req.Method, req.RequestURI, req.Proto))
 
             r = append(r, "Headers:")
@@ -196,7 +250,7 @@ func (handler hopHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
             }
             code = c
             r = append(r, fmt.Sprintf("Returning code %d", code))
-        case "-size":
+        case "-rsize":
             if len(cmd) != 2 {
                 r = append(r, fmt.Sprintf("Missing parameter for %s", cmd[0]))
                 continue
@@ -209,6 +263,18 @@ func (handler hopHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
             r = append(r, fmt.Sprintf("Appending %d bytes", b))
             r = append(r, strings.Repeat("X", b))
             r = append(r, "\n")
+        case "-size":
+            if len(cmd) != 2 {
+                r = append(r, fmt.Sprintf("Missing parameter for %s", cmd[0]))
+                continue
+            }
+            b, err := strconv.Atoi(cmd[1])
+            if err != nil {
+                r = append(r, fmt.Sprintf("Cannot create a byte array of %s bytes", cmd[1]))
+                continue
+            }
+            size = b
+            r = append(r, fmt.Sprintf("Will add %d bytes to the following request", size))
         case "-not":
             not = !not
         case "-on":
@@ -271,49 +337,79 @@ func (handler hopHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
     if skip {
         r = append(r, fmt.Sprintf("Skipping call to %s", next_cmd))
-    } else if len(next_cmd) > 0 {
-        url := fmt.Sprintf("http://%s/%s", next_cmd, path)
-        res, err := callURL(url, headers, r)
-        if err != nil {
-            r = append(r, fmt.Sprintf("Couldn't call %s. Got error: %s\n", url, err.Error()))
-        } else if res == nil {
-            r = append(r, fmt.Sprintf("Couldn't call %s by some reason\n", url))
+        if code == 0 {
+            code = 200
         }
-        if res != nil {
-            r = append(r, fmt.Sprintf("Called %s with status %s", url, res.Status))
-            if code == 0 {
-                code = res.StatusCode
-            }
-            if res.Body != nil {
-                data, err := ioutil.ReadAll(res.Body)
-                res.Body.Close()
-                if err != nil {
-                    r = append(r, err.Error())
-                    r = append(r, "\n")
+    }
+    for !skip && len(next_cmd) > 0 {
+
+        next_host, err := url.PathUnescape(next_cmd)
+        if err != nil {
+            r = append(r, fmt.Sprintf("Cannot call %s: %s\n", next_cmd, err.Error()))
+            break
+        }
+        if !strings.HasPrefix(next_host, "http://") && !strings.HasPrefix(next_host, "https://") {
+            next_host = "http://" + next_host
+        }
+        u, err := url.Parse(fmt.Sprintf("%s/%s", next_host, path))
+        if err != nil {
+            r = append(r, fmt.Sprintf("Cannot call %s: %s\n", next_host, err.Error()))
+            break
+        }
+        if u == nil {
+            break
+        }
+        if len(u.Scheme) == 0 {
+            u.Scheme = "http"
+        }
+        var res *http.Response
+        res, err = callURL(u, &headers, size)
+        if err != nil {
+            r = append(r, fmt.Sprintf("Couldn't call %s: %s\n", u, err.Error()))
+            break
+        } else if res == nil {
+            r = append(r, fmt.Sprintf("Couldn't call %s by some reason\n", u))
+            break
+        }
+        r = append(r, fmt.Sprintf("Called %s with status %s", u, res.Status))
+        if code == 0 {
+            code = res.StatusCode
+        }
+        if res.Body != nil {
+            var data []byte
+            data, err = ioutil.ReadAll(res.Body)
+            res.Body.Close()
+            if err != nil {
+                r = append(r, err.Error())
+                r = append(r, "\n")
+            } else {
+                r = append(r, "With data:")
+                if len(data) > 2048 {
+                    r = append(r, fmt.Sprintf(".\t<%d bytes>", len(data)))
                 } else {
-                    r = append(r, "With data:")
                     for _, line := range strings.Split(string(data), "\n") {
                         r = append(r, fmt.Sprintf(".\t%s", line))
                     }
                 }
             }
-            for _, h := range fheaders {
-                v := res.Header.Get(h)
-                r = append(r, fmt.Sprintf("Back forwarding header %s: %s", h, v))
-                if len(v) > 0 {
-                    rheaders[h] = v
-                }
+        }
+        for _, h := range fheaders {
+            v := res.Header.Get(h)
+            r = append(r, fmt.Sprintf("Back forwarding header %s: %s", h, v))
+            if len(v) > 0 {
+                rheaders[h] = v
             }
-            if showHeaders {
-                r = append(r, "With headers:")
-                for h, v := range res.Header {
-                    r = append(r, fmt.Sprintf(".\t%s: %s", h, v))
-                }
+        }
+        if showHeaders {
+            r = append(r, "With headers:")
+            for h, v := range res.Header {
+                r = append(r, fmt.Sprintf(".\t%s: %s", h, v))
             }
         }
         if code == 0 && err != nil {
             code = 500
         }
+        break
     }
 
     for h, v := range rheaders {
@@ -330,35 +426,96 @@ func (handler hopHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
     }
 }
 
+var port_http, port_https string
+var cacert, certificate, key string
+var verbose = false
+var useTLS = false
+
+func init() {
+    rand.Seed(13)
+    port_http = os.Getenv("PORT");
+    port_https = os.Getenv("PORT_HTTPS");
+    if len(port_http) == 0 {
+        port_http = "8000"
+    }
+    if len(port_https) == 0 {
+        port_https = "8443"
+    }
+    flag.BoolVar(&verbose, "verbose", false, "verbose output")
+    flag.StringVar(&port_http, "port_http", port_http, "port HTTP")
+    flag.StringVar(&port_https, "port_https", port_https, "port HTTPS")
+    flag.StringVar(&cacert, "cacert", "", "CA certificate")
+    flag.StringVar(&certificate, "cert", "", "client certificate")
+    flag.StringVar(&key, "key", "", "client key")
+    flag.Parse()
+
+    if verbose {
+        log.SetOutput(os.Stdout)
+    }
+
+    useTLS = len(cacert) != 0 && len(certificate) !=0 && len(key) != 0
+
+    if useTLS {
+        initTLS(cacert, certificate, key)
+    }
+}
 
 func main() {
-    rand.Seed(13)
-    port := os.Getenv("PORT");
-    if len(port) == 0 {
-        port = "8000"
-    }
-    fmt.Println("Serving on", port)
 
     s := &http.Server {
-        Addr: net.JoinHostPort("", port),
+        Addr: net.JoinHostPort("", port_http),
         Handler: hopHandler {},
         ReadTimeout: 10 * time.Second,
         WriteTimeout: 10 * time.Second,
         MaxHeaderBytes: 1 << 20,
+        ErrorLog: log.New(os.Stdout, "http: ", 0),
+    }
+
+    var stls *http.Server
+
+    if useTLS {
+        stls = &http.Server {
+            Addr: net.JoinHostPort("", port_https),
+            Handler: hopHandler {},
+            ReadTimeout: 10 * time.Second,
+            WriteTimeout: 10 * time.Second,
+            MaxHeaderBytes: 1 << 20,
+            TLSConfig: tlsServerConfig,
+            ErrorLog: log.New(os.Stdout, "https: ", 0),
+        }
     }
 
     go func() {
+        fmt.Println("Serving on", port_http)
         fmt.Println(s.ListenAndServe())
-        close(quit)
+        quit<-3
     }()
+
+    if stls != nil {
+        go func() {
+            fmt.Println("Serving on", port_https)
+            fmt.Println(stls.ListenAndServeTLS(certificate, key))
+            quit<-4
+        }()
+    }
 
     switch <-quit {
     case 1:
         fmt.Println("Shutting down")
         err := s.Shutdown(context.Background());
-        <-quit
         if err != nil {
-            panic(err)
+            fmt.Printf("Error:", err)
+        }
+        <-quit
+        if stls != nil {
+            err = stls.Shutdown(context.Background());
+            if err != nil {
+                fmt.Printf("Error:", err)
+            }
+            <-quit
+        }
+        if err != nil {
+            panic("Failed to stop gracefully")
         }
     case 2:
         panic("Rabbits are coming!")
